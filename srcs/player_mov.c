067e349579793ca24de72552b274f7831c0f0b4a
frenzla <alex.larose.sf@gmail.com>
2024-06-24 17:01:29 +0200

    implemented inactive exit animation

--- srcs/player_mov.c
+++ srcs/player_mov.c
@@ -6,7 +6,7 @@
 /*   By: alarose <alarose@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 09:31:06 by alarose           #+#    #+#             */
-/*   Updated: 2024/06/24 12:06:44 by alarose          ###   ########.fr       */
+/*   Updated: 2024/06/24 16:48:52 by alarose          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,11 +45,19 @@ int	go_right(t_data *data, int i_img)
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[data->map.move_out_i_img].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
 
 	data->map.move_in = data->map.map_layout[data->img[i_img].y][data->img[i_img].x + 1];
+	(data->img[i_img].x)++;
 	if (data->map.move_in == '0' || data->map.move_in == 'C')
 		data->map.move_out_i_img = BG;
+	else if (data->map.move_in == 'E' && data->map.nb_collectibles == 0)
+	{
+		//WIN function
+	}
 	else if (data->map.move_in == 'E' && data->map.nb_collectibles != 0)
-		data->map.move_out_i_img = EXIT;
-	(data->img[i_img].x)++;
+		{
+			data->map.move_out_i_img = EXIT;
+			mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[PLAYER_EXIT].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
+			return (1);
+		}
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[i_img].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
 	return (1);
 }
@@ -61,11 +69,19 @@ int	go_left(t_data *data, int i_img)
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[data->map.move_out_i_img].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
 
 	data->map.move_in = data->map.map_layout[data->img[i_img].y][data->img[i_img].x - 1];
+	(data->img[i_img].x)--;
 	if (data->map.move_in == '0' || data->map.move_in == 'C')
 		data->map.move_out_i_img = BG;
+	else if (data->map.move_in == 'E' && data->map.nb_collectibles == 0)
+	{
+		//WIN function
+	}
 	else if (data->map.move_in == 'E' && data->map.nb_collectibles != 0)
-		data->map.move_out_i_img = EXIT;
-	(data->img[i_img].x)--;
+		{
+			data->map.move_out_i_img = EXIT;
+			mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[PLAYER_EXIT].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
+			return (1);
+		}
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[i_img].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
 	return (1);
 }
@@ -77,11 +93,19 @@ int	go_up(t_data *data, int i_img)
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[data->map.move_out_i_img].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
 
 	data->map.move_in = data->map.map_layout[data->img[i_img].y - 1][data->img[i_img].x];
+	(data->img[i_img].y)--;
 	if (data->map.move_in == '0' || data->map.move_in == 'C')
 		data->map.move_out_i_img = BG;
+	else if (data->map.move_in == 'E' && data->map.nb_collectibles == 0)
+	{
+		//WIN function
+	}
 	else if (data->map.move_in == 'E' && data->map.nb_collectibles != 0)
-		data->map.move_out_i_img = EXIT;
-	(data->img[i_img].y)--;
+		{
+			data->map.move_out_i_img = EXIT;
+			mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[PLAYER_EXIT].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
+			return (1);
+		}
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[i_img].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
 	return (1);
 }
@@ -93,11 +117,19 @@ int	go_down(t_data *data, int i_img)
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[data->map.move_out_i_img].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
 
 	data->map.move_in = data->map.map_layout[data->img[i_img].y + 1][data->img[i_img].x];
+	(data->img[i_img].y)++;
 	if (data->map.move_in == '0' || data->map.move_in == 'C')
 		data->map.move_out_i_img = BG;
+	else if (data->map.move_in == 'E' && data->map.nb_collectibles == 0)
+	{
+		//WIN function
+	}
 	else if (data->map.move_in == 'E' && data->map.nb_collectibles != 0)
-		data->map.move_out_i_img = EXIT;
-	(data->img[i_img].y)++;
+		{
+			data->map.move_out_i_img = EXIT;
+			mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[PLAYER_EXIT].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
+			return (1);
+		}
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img[i_img].img, data->img[i_img].x * TILE_SIZE, data->img[i_img].y * TILE_SIZE);
 	return (1);
 }
